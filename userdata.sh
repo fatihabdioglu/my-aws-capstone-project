a41c23b34e27375ac5f704311cb15cc4eedcd39a
Fatih Abdioğlu <108101221+fatihabdioglu@users.noreply.github.com>
2023-09-16 19:53:12 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN="github-token-girilecek"
 git clone https://$TOKEN@github.com/fatihabdioglu/my-aws-capstone-project.git
 cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
